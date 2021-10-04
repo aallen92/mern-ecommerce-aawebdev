3371aafd6848fb82473f54390b37e5d1d9846ffb
aallen92 <azallen@icloud.com>
2021-10-04 21:24:59 +0100

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
 
@@ -7,6 +8,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/products', productRoutes)
