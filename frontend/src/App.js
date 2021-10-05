9a34188defc49db68b5e5db2f8d1a67e876236d5
aallen92 <azallen@icloud.com>
2021-10-05 21:09:59 +0100

    authentication frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import PrivateScreen from './screens/PrivateScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
+import ResetPasswordScreen from './screens/ResetPasswordScreen';
 
 // Components
 import Navbar from './components/Navbar';
 import Backdrop from './components/Backdrop';
 import SideDrawer from './components/SideDrawer';
+
+// Routing
+import PrivateRoute from './components/routing/PrivateRoute';
 
 function App() {
 
@@ -24,9 +32,14 @@ function App() {
       <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
       <main>
         <Switch>
+          <PrivateRoute exact path="/private" component={PrivateScreen} />
           <Route exact path="/" component={HomeScreen} />
           <Route exact path="/product/:id" component={ProductScreen} />
           <Route exact path="/cart" component={CartScreen} />
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/register" component={RegisterScreen} />
+          <Route exact path="/forgotpassword" component={ForgotPasswordScreen} />
+          <Route exact path="/passwordreset/:resetToken" component={ResetPasswordScreen} />
         </Switch>
       </main>
       {/* HomeScreen */}
